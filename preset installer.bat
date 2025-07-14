25b9c4c95829083ea5721ba1129dce97f41a7725
melonmat <87057824+melonmat@users.noreply.github.com>
2025-07-14 14:27:50 +0900

    Update preset installer.bat

--- preset installer.bat
+++ preset installer.bat
@@ -1,5 +1,5 @@
 @echo off
-pip install opencv-python==4.9.0.80 numpy==1.13.5 tensorflow==2.12.0 mediapipe==0.10.5 CodingRider keyboard
+pip install opencv-python==4.9.0.80 numpy==1.23.5 tensorflow==2.12.0 mediapipe==0.10.5 CodingRider keyboard
 echo.
 echo All tasks completed
 pause
